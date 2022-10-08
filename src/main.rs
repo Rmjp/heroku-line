4faadba4760f97ce2f229fc3ce8ca76be453f117
Rmjp <rmjp15.rmt@gmail.com>
2022-10-08 19:09:34 +0700

    add local

--- src/main.rs
+++ src/main.rs
@@ -16,13 +16,14 @@ async fn manual_hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    print!("Hello world!");
     HttpServer::new(|| {
         App::new()
             .service(hello)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind("[::1]:8080")?
     .run()
     .await
 }

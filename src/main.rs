1a596b52ee84d504fa4e2c151ff319fe1b859873
Rmjp <rmjp15.rmt@gmail.com>
2022-10-09 15:07:56 +0700

    succes: line_text

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use json;
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -9,6 +10,19 @@ async fn hello() -> impl Responder {
 async fn echo(req_body: String) -> impl Responder {
     println!("{}",req_body);
     HttpResponse::Ok().body(req_body)
+}
+
+#[post("/test")]
+async fn test(req_body: String) -> impl Responder {
+    let result = json::object!{
+    text: {
+        text: [
+            "test test hello"
+                ]
+        },
+        platform: "LINE"
+    };
+    HttpResponse::Ok().body(json::stringify(json::object!{ fulfillmentMessages: [result] }))
 }
 
 async fn manual_hello() -> impl Responder {
@@ -23,6 +37,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .service(hello)
             .service(echo)
+            .service(test)
             .route("/hey", web::get().to(manual_hello))
     })
     .bind(("0.0.0.0", PORT))?

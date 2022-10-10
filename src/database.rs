c06d559685a9e1647a72ae84e04fcce81ab965f0
Rmjp <rmjp15.rmt@gmail.com>
2022-10-10 20:57:46 +0700

    mysql

---
+++ src/database.rs
@@ -0,0 +1,12 @@
+use mysql::*;
+use mysql::prelude::*;
+
+pub fn connec_database(url: String) -> PooledConn{
+    let builder = OptsBuilder::from_opts(Opts::from_url(&url).unwrap());
+    let pool = Pool::new(builder.ssl_opts(SslOpts::default())).unwrap();
+    let _conn = pool.get_conn().unwrap();
+    println!("Successfully connected to PlanetScale!");
+    return _conn;
+}
+
+pub fn 
